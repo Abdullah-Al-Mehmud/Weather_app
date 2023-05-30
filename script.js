97762e38061fc83bd33ba2758456af31186e0fdb
mehmud <timehmud121@gmail.com>
2023-05-30 22:29:39 +0600

    changed ui and functionality

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const apiKey = "328b39f3f54ebc35bfeab750e5be9559";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const search = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weatherIcon");
+
+// check the weather
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`); // fetching api
   const data = await response.json();
-  console.log(data);
-  document.querySelector(".temp").innerHTML = `${data.main.temp}
-  °c`;
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".humidity").innerHTML = `${data.main.humidity} %`;
-  document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    console.log(data);
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity} %`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+
+    // weather conditions
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "img/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "img/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "img/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "img/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "img/rain.png";
+    } else if (data.weather[0].main == "Haze") {
+      weatherIcon.src = "img/haze.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "img/snow.png";
+    }
+
+    // blocking the display
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
-checkWeather();
+// it will search when you click on search button
+searchBtn.addEventListener("click", () => {
+  checkWeather(search.value);
+});
